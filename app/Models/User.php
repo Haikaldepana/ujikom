e82b5ee4711c82579ec5cd055e9ef0cf75427c6d
Haikaldepana <uncekdimz9@gmail.com>
2023-12-04 09:48:50 +0700

    haikal ganteng

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,11 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
+        'ID',
+        'username',
         'password',
+        'nama_lengkap',
+        'jabatan'
     ];
 
     /**
@@ -42,4 +44,5 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+    protected $table = "petugas";
 }
